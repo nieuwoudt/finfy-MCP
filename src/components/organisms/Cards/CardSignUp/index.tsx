e303e8e8a94fab3604f93bdcc3d16e88f4afb6b3
Nazar-D-Aleannlab <nazardubovyk@aleannlab.com>
2024-09-26 17:43:55 +0300

    feat: added check for registered user

--- src/components/organisms/Cards/CardSignUp/index.tsx
+++ src/components/organisms/Cards/CardSignUp/index.tsx
@@ -9,8 +9,8 @@ import { createAccountAction } from "@/lib/supabase/actions";
 import { Loader2 } from "lucide-react";
 import { useAppDispatch } from "@/lib/store/hooks";
 import { createUser } from "@/lib/store/features/user/userSlice";
+import { getErrorMessage } from "@/utils/helpers";
 // import {d} from 'cookies-next';
-
 
 const CardSignUp = () => {
   const router = useRouter();
@@ -19,15 +19,22 @@ const CardSignUp = () => {
   const handleClickSignUpButton = (formData: FormData) => {
     startTransition(async () => {
       const email = formData.get("email") as string;
-      
-      const { errorMessage } = await createAccountAction(formData);
-
-      if (errorMessage) {
-        toast.error(errorMessage);
-      } else {
-        dispatch(createUser({ email }));
-        router.push("/confirm-email");
-        toast.success("A verification link has been sent to your email!");
+      const ERROR_DUPLICATE_CODE = "23505";
+      try {
+        const data: any = await dispatch(createUser({ email }));
+        if (data?.error?.code !== ERROR_DUPLICATE_CODE) {
+          const { errorMessage } = await createAccountAction(formData);
+          if (errorMessage) {
+            toast.error(errorMessage);
+          } else {
+            router.push("/confirm-email");
+            toast.success("A verification link has been sent to your email!");
+          }
+        } else {
+          toast.error("This user is already registered");
+        }
+      } catch (error) {
+        toast.error(getErrorMessage(error));
       }
     });
   };
