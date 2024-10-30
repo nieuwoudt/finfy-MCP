0c9b30651a886ffb48f73f994c2a96a706b97746
Alex <oleksii.stupak@aleannlab.com>
2024-10-30 19:17:30 +0200

    feat: add bg cover for menu

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,6 @@
+'use client'
 import { Sidebar } from "@/components/organisms";
+import { useSidebar } from "@/hooks";
 import { ReactNode } from "react";
 
 export default function RootLayout({
@@ -6,9 +8,12 @@ export default function RootLayout({
 }: Readonly<{
   children: ReactNode;
 }>) {
+  const { open, handleToggle } = useSidebar();
+
   return (
     <main className="flex w-full min-h-screen font-inter">
       <Sidebar />
+      {open && <div className="flex-grow lg:hidden z-20 opacity-50 h-screen top-0 bottom-0 right-0 left-0 absolute flex bg-black"/>}
       <div className="flex-grow flex bg-navy-25">{children}</div>
     </main>
   );
