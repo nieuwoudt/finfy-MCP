9cfcc9bcd609b2ec29417c5225ce776bb407e138
ivanDAleannlab <ivand@aleannlab.com>
2024-12-03 11:55:44 +0200

    PROJ-24-terms-and-policy changed UI of dialog content

--- src/components/templates/SettingsSection/index.tsx
+++ src/components/templates/SettingsSection/index.tsx
@@ -95,11 +95,11 @@ const SettingsSection = () => {
             </Dialog.Trigger>
             <Dialog.Portal>
               <Dialog.Overlay className="fixed inset-0 bg-black/50" />
-              <Dialog.Content className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 rounded-md shadow-lg w-[80%] max-w-lg h-[400px] overflow-y-scroll">
+              <Dialog.Content className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 rounded-md shadow-lg w-[80%] max-w-lg ">
                 <Dialog.Close className="absolute top-2 right-2">
                   ✕
                 </Dialog.Close>
-                <div>{renderContent()}</div>
+                <div className="h-[400px] overflow-y-scroll">{renderContent()}</div>
               </Dialog.Content>
             </Dialog.Portal>
           </Dialog.Root>
