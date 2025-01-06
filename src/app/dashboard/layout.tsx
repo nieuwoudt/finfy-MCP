1a976ce7b2a1f78410e252e3d373596930badb97
Alex <oleksii.stupak@aleannlab.com>
2025-01-06 13:36:44 +0200

    fix: overflow

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   const { open, handleToggle } = useSidebar();
 
   return (
-    <main className="flex w-full min-h-screen font-inter">
+    <main className="flex w-full overflow-hidden min-h-screen fixed md:static top-0 bottom-0 left-0 right-0 font-inter">
       <Sidebar />
       {open && <div className="flex-grow lg:hidden z-20 opacity-50 h-screen top-0 bottom-0 right-0 left-0 absolute flex bg-black"/>}
       <div className="flex-grow flex bg-navy-25">{children}</div>
--- src/components/atoms/Textarea/index.tsx
+++ src/components/atoms/Textarea/index.tsx
@@ -13,6 +13,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
           "flex w-full rounded-full border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
+        autoFocus={false}
         ref={ref}
         {...props}
       />
